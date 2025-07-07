cda199195fce458ac8b6ebc1cfeab747a6c07794
Gary Behan <gary.b@browserstack.com>
2025-07-07 11:02:08 +0100

    Initial commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,53 @@
+// @ts-check
+export {};
+const { test, expect } = require('@playwright/test');
+const percySnapshot = require('@percy/playwright');
+
+var username = "student";
+var wrongUsername = "wrongStudent";
+var password = "Password123";
+var wrongPassword = "Password321";
+var wrongUsernameText = "Your username is invalid!";
+var wrongPasswordText = "Your password is invalid!";
+
+test('A successful login was performed', async ({ page } : { page:any }) => {
+  console.log(`[[PROPERTY|id=TC-787985]]`);
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+  
+
+  await page.locator("#username").fill(username);
+  await page.locator("#password").fill(wrongPassword);
+  await page.locator("#submit").click();
+
+  await percySnapshot(page, 'Login Page');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveURL("https://practicetestautomation.com/logged-in-successfully/");
+
+  await percySnapshot(page, 'Login Successful');
+});
+
+test('The wrong username was entered', async ({ page } : { page:any }) => {
+  console.log(`[[PROPERTY|id=TC-787987]]`);
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+  await page.locator("#username").fill(wrongUsername);
+  await page.locator("#password").fill(password);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#error")).toHaveText(wrongUsernameText);
+});
+
+test('The wrong password was entered', async ({ page } : { page:any }) => {
+  console.log(`[[PROPERTY|id=TC-787986]]`);
+  await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+  await page.locator("#username").fill(username);
+  await page.locator("#password").fill(wrongPassword);
+  await page.locator("#submit").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.locator("#error")).toHaveText(wrongPasswordText);
+});
